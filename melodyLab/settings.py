407bd5da15c11b25f6ea51717554a9c3315821eb
Harry-GRO <groveshj06@gmail.com>
2024-01-25 21:10:44 +0000

    something changed apparently

--- melodyLab/settings.py
+++ melodyLab/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'melodyLab.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
